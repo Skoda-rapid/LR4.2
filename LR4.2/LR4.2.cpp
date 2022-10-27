f77b6efefc67e8f197ca668613753bc96a5881fc
Ivan <ivan.turash.ri.2022@lpnu.ua>
2022-10-28 00:36:51 +0300

    commit_lr4.2

---
+++ LR4.2/LR4.2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = abs(x * x * x);
+		if (x < -1)
+			B = abs(2 + x) + sin(x) * sin(x);
+		else
+			if (x > 0.1)
+				B = atan(x * x * x + 1) + 1;
+			else
+				B = exp(cos(x)) + log10(1 / x + 1);
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	system("pause");
+	return 0;
+
+	
+}
